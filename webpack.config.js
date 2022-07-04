d991c5219acab14cea417e649890a350aa2656e8
Aldo Salas <aldosalasrdz@gmail.com>
2022-07-04 15:48:57 -0500

    React con CSS y Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,67 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true
-  },
-  mode: 'production',
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, argv) => {
+  const { mode } = argv
+  const isProduction = mode === 'production'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction
+        ? '[name].[contenthash].js'
+        : 'main.js',
+      clean: !!isProduction
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader'
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
         }
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './public/index.html',
-      filename: './index.html'
-    })
-  ]
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './public/index.html',
+        filename: './index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      })
+    ],
+    devServer: {
+      open: true
+    }
+  }
 }
